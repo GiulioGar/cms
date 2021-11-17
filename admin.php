e2c76742c23a038e5224c2b8a1415e6acaba1f0b
Giulio Garofalo <g.garofalo@interactive-mr.com>
2021-11-17 17:25:07 +0100

    bug1711

--- admin.php
+++ admin.php
@@ -9,7 +9,8 @@ $database_admin = "millebytesdb";
 $username_admin = "mbuser";
 $password_admin = '$leeple%1598';
 //$admin = mysql_pconnect($hostname_admin, $username_admin, $password_admin) or trigger_error(mysql_error(),E_USER_ERROR); 
-
+$connnessione="test";
+$cn=strpos()
 //$con = @mysqli_connect('46.37.21.33', 'mbuser', '$leeple%1598', 'millebytesdb');
 $con = @mysqli_connect('localhost', 'mbuser', '$leeple%1598', 'millebytesdb');
 //$admin = @mysqli_connect('46.37.21.33', 'mbuser', '$leeple%1598', 'millebytesdb');
@@ -23,4 +24,7 @@ if (!$con) {
     echo "Error: " . mysqli_connect_error();
 	exit();
 }
+
+
+
 ?>
